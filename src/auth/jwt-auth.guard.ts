801d0fb5c2d4f164498e3cb01bb4ec1f62b0f1f3
Efemax00 <davidoders788@gmail.com>
2026-01-09 14:56:33 +0100

    feat: directed auth gaurd to use jwt

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,27 +1,5 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard implements CanActivate {
-  constructor(private jwt: JwtService) {}
-
-  async canActivate(ctx: ExecutionContext) {
-    const req = ctx.switchToHttp().getRequest();
-    const token = req.headers.authorization?.split(' ')[1];
-
-    if (!token) throw new UnauthorizedException();
-
-    try {
-      const payload = await this.jwt.verifyAsync(token);
-      req.user = payload;
-      return true;
-    } catch {
-      throw new UnauthorizedException();
-    }
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
