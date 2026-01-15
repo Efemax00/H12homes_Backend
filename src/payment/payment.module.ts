70fee67e7341b826840750a8af471ed66571f0d5
Efemax00 <davidoders788@gmail.com>
2026-01-15 14:39:55 +0100

    fix:updated payment module to include cloudinary service

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -2,9 +2,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentController } from '../payment/payment.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [PaymentController],
+  providers: [CloudinaryService],
 })
 export class PaymentModule {}
